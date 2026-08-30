81be1fa4740a6cf9a3ff2ceb5ff396cdaa71424e
Benni <bennitir@gmail.com>
2026-08-30 21:54:27 +0000

    Register service worker for GLÖGGT mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 import MobileAwareShell from "@/components/MobileAwareShell";
 import BackNavigationRefresh from "@/components/BackNavigationRefresh";
 import TopClock from "@/components/TopClock";
@@ -212,8 +213,8 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full">
-        <BackNavigationRefresh />
-
+  <ServiceWorkerRegister />
+  <BackNavigationRefresh />
         {plainPage ? (
           children
         ) : (
