e41cea00bd87b0d983920352fa9841dcb05511f6
Benni <bennitir@gmail.com>
2026-08-30 21:39:05 +0000

    Scope GLÖGGT mobile PWA

--- app/manifest.ts
+++ app/manifest.ts
@@ -6,6 +6,7 @@ export default function manifest(): MetadataRoute.Manifest {
     short_name: "GLÖGGT",
     description: "GLÖGGT Mobile",
     start_url: "/mobile",
+    scope: "/mobile",
     display: "standalone",
     background_color: "#ffffff",
     theme_color: "#ffffff",
